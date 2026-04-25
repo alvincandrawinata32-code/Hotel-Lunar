2314d8867b09019d5da974db190df9d07f578be6
alvincandrawinata32-code <alvincandrawinata32@gmail.com>
2026-04-25 19:27:59 +0700

    nambah file

---
+++ login_office/php/index.php
@@ -0,0 +1,30 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <title>Data User</title>
+</head>
+<body>
+    <h2>Data User</h2>
+    <a href="tambah.php">Tambah Data</a>
+
+    <table border="1" cellpadding="10">
+
+    <tr>
+        <th>Nama</th>
+        <th>Email</th>
+        <th>Password</th>
+        <th>Aksi</th>
+    </tr>
+
+    <tr>
+        <td>John Doe</td>
+        <td>john.doe@example.com</td>
+        <td>********</td>
+        <td>
+            <a href="edit.php">Edit</a> |
+            <a href="hapus.php">Hapus</a>
+        </td>
+    </tr>
+    </table>
+</body>
+</html>
