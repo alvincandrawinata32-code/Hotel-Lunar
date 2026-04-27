f0434066ad91558f9595b443c3fd104a8f5614f3
alvincandrawinata32-code <alvincandrawinata32@gmail.com>
2026-04-27 14:41:09 +0700

    nambah file + update fitur

--- PHP/db.php
+++ PHP/db.php
@@ -4,11 +4,10 @@ $username = "root";
 $password = "";
 $dbname = "hotel";
 
-try{
+try {
     $conn = new PDO("mysql:host=$servername;dbname=$dbname", $username, $password);
-    $conn->setAttribute(PDO::ATR_ERRMODE, value:PD0::ERRMODE_EXCEPTION);
-}
-catch(PD0Exception $e){
-    echo "Koneksi gagal: ". $e->getMassage();
+    $conn->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
+} catch(PDOException $e) {
+    echo "Koneksi gagal: " . $e->getMessage();
 }
 ?>
