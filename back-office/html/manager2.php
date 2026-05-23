9bc661369dc3faaa2ac9d4044126150f4b659686
alvincandrawinata32-code <alvincandrawinata32@gmail.com>
2026-05-24 00:43:46 +0700

    update code

--- back-office/html/manager2.php
+++ back-office/html/manager2.php
@@ -8,7 +8,7 @@ require_once __DIR__ . '/../../PHP/db.php';
 
 $stmt = $conn->query("
     SELECT r.id_reservasi, t.username, t.id_tamu, t.alamat, k.tipe_kamar,
-           r.tanggal_checkin, r.tanggal_checkout, p.total_bayar, p.metode_bayar
+    r.tanggal_checkin, r.tanggal_checkout, p.total_bayar, p.metode_bayar
     FROM reservasi r
     JOIN tamu t ON r.id_tamu = t.id_tamu
     JOIN kamar k ON r.id_kamar = k.id_kamar
